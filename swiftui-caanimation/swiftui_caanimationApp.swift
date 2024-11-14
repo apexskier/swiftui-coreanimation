34a93a6c42672f3fb9f68e3b0793c89413fb73d8
Cameron Little <cameron@camlittle.com>
2024-11-14 12:26:34 -0700

    Initial Commit

---
+++ swiftui-caanimation/swiftui_caanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_caanimationApp.swift
+//  swiftui-caanimation
+//
+//  Created by Cameron Little on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_caanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
